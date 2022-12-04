5e243acd6aaebda43c4d74f9355c61de6dd27180
myan <myan@redhat.com>
2022-12-04 02:19:05 +0000

    check managed cluster for each controlplane in the e2e test
    Signed-off-by: myan <myan@redhat.com>

--- test/e2e/managed_cluster_test.go
+++ test/e2e/managed_cluster_test.go
@@ -13,34 +13,53 @@ import (
 
 var _ = ginkgo.Describe("ManagedCluster", func() {
 	var runtimeClient1 runtimeClient.Client
+	var runtimeClient2 runtimeClient.Client
 
 	ginkgo.BeforeEach(func() {
 		ginkgo.By("Check the option controlplane")
-		gomega.Expect(len(options.ControlPlanes) > 0).Should(gomega.BeTrue())
-		gomega.Expect(len(options.ControlPlanes[0].ManagedCluster) > 0).Should(gomega.BeTrue())
+		gomega.Expect(len(options.ControlPlanes) == 2).Should(gomega.BeTrue())
+		gomega.Expect(len(options.ControlPlanes[0].ManagedCluster) == 1).Should(gomega.BeTrue())
+		gomega.Expect(len(options.ControlPlanes[1].ManagedCluster) == 1).Should(gomega.BeTrue())
 
-		ginkgo.By("Get a controlplane name")
-
-		ginkgo.By("Get the controlplane restConfig")
-		restConfig, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-			&clientcmd.ClientConfigLoadingRules{
-				ExplicitPath: options.ControlPlanes[0].KubeConfig,
-			},
-			&clientcmd.ConfigOverrides{
-				CurrentContext: options.ControlPlanes[0].Context,
-			}).ClientConfig()
+		ginkgo.By("Get the clients of controlplanes")
+		var err error
+		runtimeClient1, err = getRuntimeClient(options.ControlPlanes[0].KubeConfig, options.ControlPlanes[0].Context)
 		gomega.Expect(err).Should(gomega.BeNil())
-
-		ginkgo.By("Get the controlplane runtime client")
-		runtimeClient1, err = runtimeClient.New(restConfig, runtimeClient.Options{Scheme: runtimeScheme})
+		runtimeClient2, err = getRuntimeClient(options.ControlPlanes[1].KubeConfig, options.ControlPlanes[1].Context)
 		gomega.Expect(err).Should(gomega.BeNil())
 	})
 
-	ginkgo.It("get managed cluster from controlplane", func() {
-		managedCluster := clusterv1.ManagedCluster{}
-		managedCluster.SetName(options.ControlPlanes[0].ManagedCluster[0].Name)
-		err := runtimeClient1.Get(context.TODO(), runtimeClient.ObjectKeyFromObject(&managedCluster), &managedCluster)
+	ginkgo.It("get managed cluster from controlplanes", func() {
+		ginkgo.By("Check the managed cluster from controlplane1")
+		managedCluster1 := clusterv1.ManagedCluster{}
+		managedCluster1.SetName(options.ControlPlanes[0].ManagedCluster[0].Name)
+		err := runtimeClient1.Get(context.TODO(), runtimeClient.ObjectKeyFromObject(&managedCluster1), &managedCluster1)
 		gomega.Expect(err).Should(gomega.BeNil())
-		gomega.Expect(len(managedCluster.GetUID()) > 0).Should(gomega.BeTrue())
+		gomega.Expect(len(managedCluster1.GetUID()) > 0).Should(gomega.BeTrue())
+
+		ginkgo.By("Check the managed cluster from controlplane2")
+		managedCluster2 := clusterv1.ManagedCluster{}
+		managedCluster2.SetName(options.ControlPlanes[1].ManagedCluster[0].Name)
+		err = runtimeClient2.Get(context.TODO(), runtimeClient.ObjectKeyFromObject(&managedCluster2), &managedCluster2)
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(len(managedCluster2.GetUID()) > 0).Should(gomega.BeTrue())
 	})
 })
+
+func getRuntimeClient(kubeconfig, kubecontext string) (runtimeClient.Client, error) {
+	restConfig, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{
+			ExplicitPath: kubeconfig,
+		},
+		&clientcmd.ConfigOverrides{
+			CurrentContext: kubecontext,
+		}).ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+	client, err := runtimeClient.New(restConfig, runtimeClient.Options{Scheme: runtimeScheme})
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
